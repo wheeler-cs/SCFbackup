5a2a0b9757b871458c99cc8967db969d4e6bccee
Drew Wheeler <wheelercs@protonmail.com>
2023-10-24 17:58:30 -0500

    Getting Directories
    Currently able to get directories of files, but call is not recursive.

--- src/filesystem.c
+++ src/filesystem.c
@@ -1,5 +1,6 @@
 #include "filesystem.h"
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,21 +13,49 @@ unsigned int generate_file_database (char *root_directory, char **file_names)
     struct dirent *directory_info;
     struct stat file_info;
 
+    char *full_name = NULL;
+    unsigned int full_name_length = 0;
+
     // Attempt to open directory for indexing
     if ((dir_ptr = opendir (root_directory)) != NULL)
     {
         while ((directory_info = readdir (dir_ptr)) != NULL)
         {
             // Ignore . and .. directories
-            if ((abs (strcmp (directory_info->d_name, "."))) ||
-                (abs (strcmp (directory_info->d_name, ".."))))
+            if (!(abs (strcmp (directory_info->d_name, "."))) ||
+                !(abs (strcmp (directory_info->d_name, ".."))))
                 continue;
 
+            // NOTE: This is kind a jank way of doing it
+            // TODO: Make this better in the future
+            full_name_length = strlen (root_directory) + 1 + strlen (directory_info->d_name);
+            full_name = malloc (full_name_length);
+            memset (full_name, '\0', full_name_length * sizeof (char));
+            strcat (full_name, root_directory);
+            strcat (full_name, "/");
+            strcat (full_name, directory_info->d_name);
+            printf ("\n%s", full_name);
 
+
+            if ((stat (full_name, &file_info)) == 0)
+            {
+                if (S_IFDIR (file_info.st_mode))
+                {
+                    printf ("\nDirectory Found!");
+                    //generate_file_database (full_name, NULL);
+                }
+                else if (S_IFREG(file_info.st_mode))
+                {
+                    printf ("\nFile Found!");
+                }
+            }
+
+            free (full_name);
         }
 
         // Stop a memory leak
         closedir (dir_ptr);
+        printf ("\n");
     }
     else
     {
