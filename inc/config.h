62958b4b6b64eddc4e9566692da5b7f95ea5dbb4
Drew Wheeler <wheelercs@protonmail.com>
2023-11-18 20:49:51 -0600

    Work on Config Loading
    Working on loading the config file data. Only some very basic functions have been set up, with no real functionality to them.

--- inc/config.h
+++ inc/config.h
@@ -3,6 +3,11 @@
 
 #include "server.h"
 
+#include <stdio.h>
+
 void load_server_config (struct server_info *);
+void load_server_directories_config (struct server_info *, FILE *);
+void load_server_files_config (struct server_info *, FILE *);
+void load_server_network_config (struct server_info *, FILE *);
 
 #endif
--- src/server_main.c
+++ src/server_main.c
@@ -1,3 +1,4 @@
+#include "config.h"
 #include "filesystem.h"
 #include "list.h"
 #include "server.h"
@@ -10,6 +11,8 @@ int main (int argc, char **argv)
     // TEST
     struct server_info *storage_server = create_new_server();
     storage_server->configuration.data_dir = ".";
+    storage_server->configuration.config_file = "./test/server.cfg";
+    load_server_config (storage_server);
     generate_file_database (storage_server->configuration.data_dir, storage_server->file_data);
     delete_server_data (&storage_server, (void (*) (void*)) delete_file_record);
     // ENDTEST
